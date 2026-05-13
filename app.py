e6957fe5def18de3251cc1d1f69fb6e38e04864e
oyatillokadraliev-dot <oyatillokadraliev@gmail.com>
2026-05-13 21:47:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -283,3 +283,9 @@ def new_message(data):
 # =========================
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+import traceback
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    print(traceback.format_exc())
+    return "SERVER ERROR", 500
